6d1a27c22caf9660021c84fe9e1f16b8646d6781
Isaac <59330021+bytesandroses@users.noreply.github.com>
2024-04-28 09:04:11 +0200

    Can't remember the change made... should have noted it when it was made last week

--- js-file.js
+++ js-file.js
@@ -1,6 +1,26 @@
 const button = document.querySelector("button");
-
 const input = document.querySelector("input");
-
 const ul = document.querySelector("ul");
 
+function addItem() {
+  const item = input.value;
+  input.value = "";
+
+  const addItem = document.createElement("li");
+  const addSpan = document.createElement("span");
+  const addButton = document.createElement("button");
+  
+  addItem.appendChild(addSpan);
+  addItem.appendChild(addButton);
+  
+  addSpan.textContent = item;
+  addButton.textContent = "Delete";
+
+  ul.appendChild(addItem);
+
+  addButton.addEventListener("click", () => {
+    ul.removeChild(addItem);
+  });
+}
+
+button.addEventListener("click", addItem);
